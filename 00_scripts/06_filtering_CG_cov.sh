cab2042f441cc4a21c81f1e7b00aebead6bda789
Clément Rougeux <clementrougeux@rogos-hst-p00.fl.ulaval.ca>
2019-05-17 16:26:46 -0600

    Add existing file

---
+++ 00_scripts/06_filtering_CG_cov.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+  
+#SBATCH -J wgbs_filt
+#SBATCH -o log-filtering_CG_cov.out
+#SBATCH -c 1 
+#SBATCH -p small
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=clement.rougeux.1@ulaval.ca
+#SBATCH --time=1-00:00
+#SBATCH --mem=3G
+
+cd $SLURM_SUBMIT_DIR
+
+# keep some info
+TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
+SCRIPT=$0
+NAME=$(basename $0)
+LOG_FOLDER="98_log_files"
+echo "$SCRIPT"
+cp "$SCRIPT" "$LOG_FOLDER"/"$TIMESTAMP"_"$NAME"
+
+## Define variables
+RAWMETH="08_methylation_call"
+FILTMETH="10_methylation_CG"
+ID="BAL-3"
+
+## Basic filtering on coverage and on methylation type
+for file in $(ls $RAWMETH/"$ID"*.sorted.bam.dedup_reads.bam.CGmap.gz) 
+    do
+        base=$(basename $file)
+        echo 'Filtering file' $base
+        file2=$(echo "$file" | perl -pe 's/.sorted.bam.dedup_reads.bam.CGmap.gz//')
+        name=$(basename $file2)
+        echo $file        
+        gunzip -c $file | 
+            awk '$4~/CG/'| 
+            awk '$7>=10' |
+            awk '$8>=10' |
+            awk '$7<100' |
+            awk '$8<100' > "$FILTMETH"/"$name"_CG.CGmap
+done
---
+++ 00_scripts/07_filtering_CG_genetic.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+  
+#SBATCH -J wgbs_filtG
+#SBATCH -o log-filtering_CG_genet.out
+#SBATCH -c 1 
+#SBATCH -p small
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=clement.rougeux.1@ulaval.ca
+#SBATCH --time=1-00:00
+#SBATCH --mem=3G
+
+cd $SLURM_SUBMIT_DIR
+
+# keep some info
+TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
+SCRIPT=$0
+NAME=$(basename $0)
+LOG_FOLDER="98_log_files"
+echo "$SCRIPT"
+cp "$SCRIPT" "$LOG_FOLDER"/"$TIMESTAMP"_"$NAME"
+
+
+## Define variables
+GENET="09_genotype_positions/sites_all_maf0.01_pctind0.2_CTs_temp"
+METH="10_methylation_CG"
+TEMP="99_tmp"
+ID="BAL-11"
+
+## Filtering for gentic CT polymorphisms
+for file in $(ls $METH/*_CG.CGmap)
+
+    do
+        # Formatting the CGmap file
+        base=$(basename $file)
+        echo "Formating file:" $base
+        file2=$(echo "$file" | perl -pe 's/_CG.CGmap//') 
+        name=$(basename $file2)        
+        awk '{print$1$3"\t"$0}' $file > "$TEMP"/"$base".temp
+       
+        # Filtering the CGmap file
+        echo "Filtering:" $base
+        awk 'NR==FNR{a[$1];next} !($1 in a)' $GENET "$TEMP"/"$base.temp" > "$TEMP"/"$base".temp2
+        
+        # Reformating the filtered file to CGmap format
+        echo "Formating to CGmap format:" $base
+        cut -f 2-  "$TEMP"/"$base.temp2" > "$METH"/"$name"_CG_noCT.CGmap
+        
+        # Removing TEMP files
+        echo "Removed temp files for:" $name
+        rm "$TEMP"/"$base.temp"
+        rm "$TEMP"/"$base.temp2"
+
+        echo "
+        >>> "
+done
+
